255c13d5d09fcda233deb8605496c1acdacb11b8
alexeiscript <suzakukururug1@yandex.com>
2020-11-25 12:59:11 +0200

    integrating remote api calls

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
+import React, {Component} from 'react';
 import PageWrapper from './components/PageWrapper';
 import {BrowserRouter as Router, Route} from 'react-router-dom';
+import {connect} from 'react-redux';
 
 //Pages
 import Home from './components/Pages/Home';
@@ -9,68 +11,91 @@ import About from './components/Pages/About';
 import Team from './components/Pages/Team';
 import Login from './components/Pages/Login';
 import AdminWrapper from './components/AdminWrapper';
+import Dashboard from './components/Pages/Dashboard';
 
-function App() {
-  return (
-    <Router>
+class App extends Component {
+  render() {
+    return (
+      <Router>
+        <Route 
+          path="/admin"
+          render={props => {
+            return (
+              <AdminWrapper>
+                {this.props.auth.token ?
+                  <Dashboard />
+                :
+                  <Login />
+                }
+              </AdminWrapper>
+            )
+          }}
+        />
 
-      <Route 
-        path="/admin"
-        render={props => (
-          <AdminWrapper>
-            <Login />
-          </AdminWrapper>
-        )}
-      />
+        <Route 
+          exact={true}
+          path="/"
+          render={props => (
+            <PageWrapper>
+              <Home {...props} />
+            </PageWrapper>
+          )}
+        />
+      
+        <Route 
+          path="/portfolio"
+          render={props => (
+            <PageWrapper>
+              <Portfolio {...props} />
+            </PageWrapper>
+          )}
+        />
 
-      <Route 
-        exact={true}
-        path="/"
-        render={props => (
-          <PageWrapper>
-            <Home {...props} />
-          </PageWrapper>
-        )}
-      />
-    
-      <Route 
-        path="/portfolio"
-        render={props => (
-          <PageWrapper>
-            <Portfolio {...props} />
-          </PageWrapper>
-        )}
-      />
+        <Route 
+          path="/services"
+          render={props => (
+            <PageWrapper>
+              <Services {...props} />
+            </PageWrapper>
+          )}
+        />
 
-      <Route 
-        path="/services"
-        render={props => (
-          <PageWrapper>
-            <Services {...props} />
-          </PageWrapper>
-        )}
-      />
+        <Route 
+          path="/about"
+          render={props => (
+            <PageWrapper>
+              <About {...props} />
+            </PageWrapper>
+          )}
+        />
 
-      <Route 
-        path="/about"
-        render={props => (
-          <PageWrapper>
-            <About {...props} />
-          </PageWrapper>
-        )}
-      />
+        <Route 
+          path="/team"
+          render={props => (
+            <PageWrapper>
+              <Team {...props} />
+            </PageWrapper>
+          )}
+        />
 
-      <Route 
-        path="/team"
-        render={props => (
-          <PageWrapper>
-            <Team {...props} />
-          </PageWrapper>
-        )}
-      />
-
-    </Router>
-  );
+      </Router>
+    );
+  }
 }
 
-export default App;
+const mapStateToProps = state => {
+  return {
+    auth: state.auth
+  }
+}
+
+const mapDispatchToProps = dispatch => {
+  return {
+
+  }
+}
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(App);
