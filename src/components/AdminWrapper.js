87ae085a85acf6385b0fa3d9ffef6fc5786f1abf
alexeiscript <suzakukururug1@yandex.com>
2020-11-25 18:13:44 +0200

    installed material ui, applying styles

--- src/components/AdminWrapper.js
+++ src/components/AdminWrapper.js
@@ -1,10 +1,23 @@
 import React, {Component} from 'react';
 import './assets/css/admin.css';
+import AppBar from '@material-ui/core/AppBar';
+import ToolBar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
 
 class AdminWrapper extends Component {
     render() {
         return(
             <div id="admin-page">
+                <AppBar>
+                    <ToolBar>
+                        <Typography
+                            component="h1"
+                            variant="h6"
+                            color="inherit"
+                            noWrap
+                        >Admin</Typography>
+                    </ToolBar>
+                </AppBar>
                 {this.props.children}
             </div>
         );
